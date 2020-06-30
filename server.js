de3ba224cf2169ff67137d54d4a5c4e31f272ad1
Mitesh Modi <mitesh.modi003@gmail.com>
2020-06-30 12:26:46 -0400

    node modules

--- server.js
+++ server.js
@@ -1,12 +1,10 @@
 const express = require("express");
-// const logger = require("morgan");
 const mongoose = require("mongoose");
 
 const PORT = process.env.PORT || 3000;
 
 const app = express();
 
-// app.use(logger("dev"));
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 app.use(express.static("public"));
